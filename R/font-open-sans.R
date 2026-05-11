f6eb5fb4e2b17889c2327179a24008e2f3225a7f
Markus Skyttner <mskyttner@pm.me>
2026-05-11 17:22:17 +0200

    fix R 4.6.0 compatibility: remove extrafont, fix ggplot2 deprecations
    - Replace extrafont (archived CRAN Sep 2025) with systemfonts for font
      detection in .onAttach; drop extrafont::font_import() calls from
      import_* functions and extrafont::loadfonts() from zzz.R
    - Remove Remotes: wch/Rttf2pt1 (root cause of extrafont archival)
    - Rename element_line(size=) to element_line(linewidth=) in theme-kth.R
      (deprecated in ggplot2 3.4.0)
    - Remove scale_name= and rename trans= to transform= in continuous_scale()
      calls in scales.R (both deprecated in ggplot2 3.5+)
    - Add CLAUDE.md to .gitignore and .Rbuildignore

--- R/font-open-sans.R
+++ R/font-open-sans.R
@@ -1,21 +1,13 @@
 #' Import Open Sans font for use in charts
 #'
-#' There is an option `ktheme.loadfonts` which -- if set to `TRUE` -- will
-#' call `extrafont::loadfonts()` to register non-core fonts with R PDF & PostScript
-#' devices. If you are running under Windows, the package calls the same function
-#' to register non-core fonts with the Windows graphics device.
-#'
 #' @md
-#' @note This will take care of ensuring PDF/PostScript usage. The location of the
-#'   font directory is displayed after the base import is complete. It is highly
+#' @note The location of the font directory is displayed. It is highly
 #'   recommended that you install them on your system the same way you would any
 #'   other font you wish to use in other programs.
 #' @export
 import_open_sans <- function() {
 
   os_font_dir <- system.file("fonts", "open-sans", package="ktheme")
-
-  suppressWarnings(suppressMessages(extrafont::font_import(os_font_dir, prompt=FALSE)))
 
   message(
     sprintf(
